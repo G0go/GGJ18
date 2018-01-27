3748cfd88b15ea37021ff9fb81052d02c4d3af5b
Abel Vinuela-Hamoudi <abel.vinuela-hamoudi@epitech.eu>
2018-01-27 11:08:43 +0100

    merge

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -1,10 +1,17 @@
 class Menu extends Phaser.State {
 
 	preload() {
-		this.load('');
+		this.game.load.image('Menu_background', 'assets/Menu_background.jpg');
 	}
-	create() {
 
+	create() {
+		console.log('bite');
+		var sprite = this.game.add.sprite(this.game.width / 2, this.game.height / 2, 'Menu_background');
+		sprite.;
+		console.log(window.innerHeight);
+		console.log(window.innerWidth);
 	}
 
 }
+
+export default Menu;

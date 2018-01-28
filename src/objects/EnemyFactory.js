7221da3b3c3022dfaaba31af0027ebfb372d77eb
Gauthier Cler <gauthier.cler@epitech.eu>
2018-01-28 02:11:00 +0100

    cleaned enum definition EnemyFactory

--- src/objects/EnemyFactory.js
+++ src/objects/EnemyFactory.js
@@ -4,27 +4,25 @@ import Enemy from "./Enemy";
 class EnemyFactory {
 
     constructor(game) {
+        this.enemyType = {
+            SHOOTER: 0,
+            SWORDMAN: 1,
+            DOG: 2,
+        };
         this.game = game;
-        this.initEnemiesType();
         this.protos = [
-            { name: 'gunman', spell: new Spell('Shoot', 700, (self) => {}) },
-            { name: 'swordman', spell: new Spell('Hit', 300, (self) => {}) },
+            { name: 'shooter', spell: new Spell('Shoot', 700, (self) => {}) },
+            { name: 'Dashicon', spell: new Spell('Hit', 300, (self) => {}) },
             { name: 'dog', spell: new Spell('Bite', 300, (self) => {}) }
         ];
         this.enemies = [];
     }
 
-    initEnemiesType() {
-        this.GUNMAN = 0;
-        this.SWORDMAN = 1;
-        this.DOG = 2;
-    }
-
     spawn(type, position) {
-        let proto = this.enemies[type];
+        let proto = this.protos[type];
         let sprite = this.game.add.sprite(position.x, position.y, proto.name);
         let enemy = new Enemy(sprite, proto.spell);
-        this.enemies.add(enemy);
+        this.enemies.push(enemy);
         return enemy;
     }
 
